322823b448f6221d12ecf34d4c6bfe75b1e7d5fe
matthew simone <ST10272756@vcconnect.edu.za>
2024-04-16 16:49:52 +0200

    Add project files.

---
+++ PROG.POE.PART1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG.POE.PART1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
